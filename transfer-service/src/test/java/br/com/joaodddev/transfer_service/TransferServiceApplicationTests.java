7fcc178410e228bd5c6d44cee3bbb1a60f75c69f
João Victor <joaodddev@gmail.com>
2026-06-09 19:15:02 -0300

    feat: scaffold monorepo with three spring boot modules

---
+++ transfer-service/src/test/java/br/com/joaodddev/transfer_service/TransferServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joaodddev.transfer_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransferServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
