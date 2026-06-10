8a9734c8498417f80ae724640e3578163249ca5f
João Victor <joaodddev@gmail.com>
2026-06-10 12:12:08 -0300

    feat(transfer): add TransferRepository

--- transfer-service/src/main/java/br/com/joaodddev/transfer_service/repository/TransferRepository.java
+++ transfer-service/src/main/java/br/com/joaodddev/transfer_service/repository/TransferRepository.java
@@ -1,4 +1,16 @@
 package br.com.joaodddev.transfer_service.repository;
 
-public class TransferRepository {
+import br.com.joaodddev.transfer_service.domain.Transfer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface TransferRepository extends JpaRepository<Transfer, UUID> {
+
+    Optional<Transfer> findByIdempotencyKey(UUID idempotencyKey);
+
+    boolean existsByIdempotencyKey(UUID idempotencyKey);
 }
