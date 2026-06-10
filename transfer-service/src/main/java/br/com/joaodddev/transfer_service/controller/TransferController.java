9f77fa96f7b2836e86885a71075ee4291a78090b
João Victor <joaodddev@gmail.com>
2026-06-10 12:25:34 -0300

    feat(transfer): add TransferController

--- transfer-service/src/main/java/br/com/joaodddev/transfer_service/controller/TransferController.java
+++ transfer-service/src/main/java/br/com/joaodddev/transfer_service/controller/TransferController.java
@@ -1,4 +1,35 @@
 package br.com.joaodddev.transfer_service.controller;
 
+import br.com.joaodddev.transfer_service.dto.TransferRequest;
+import br.com.joaodddev.transfer_service.dto.TransferResponse;
+import br.com.joaodddev.transfer_service.service.TransferService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/transfers")
+@RequiredArgsConstructor
 public class TransferController {
+
+    private final TransferService transferService;
+
+    @PostMapping
+    public ResponseEntity<TransferResponse> transfer(@RequestBody @Valid TransferRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(transferService.transfer(request));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TransferResponse>> findAll() {
+        return ResponseEntity.ok(transferService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TransferResponse> findById(@PathVariable UUID id) {
+        return ResponseEntity.ok(transferService.findById(id));
+    }
 }
