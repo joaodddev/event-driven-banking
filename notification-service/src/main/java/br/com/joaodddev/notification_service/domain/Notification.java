9c4573b3a732ffafa6db1fed49c1b531f1f8ca74
João Victor <joaodddev@gmail.com>
2026-06-11 09:59:55 -0300

    feat(notification): add Notification entity

--- notification-service/src/main/java/br/com/joaodddev/notification_service/domain/Notification.java
+++ notification-service/src/main/java/br/com/joaodddev/notification_service/domain/Notification.java
@@ -1,4 +1,37 @@
 package br.com.joaodddev.notification_service.domain;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "notifications")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Notification {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "event_type", nullable = false, length = 50)
+    private String eventType;
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    @Column(nullable = false, columnDefinition = "jsonb")
+    private String payload;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
 }
