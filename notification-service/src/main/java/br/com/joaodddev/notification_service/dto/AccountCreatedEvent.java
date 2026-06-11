780d11495cb3bad3b09569a7fcdb3595398ecc64
João Victor <joaodddev@gmail.com>
2026-06-11 10:00:55 -0300

    feat(notification): add event DTOs

--- notification-service/src/main/java/br/com/joaodddev/notification_service/dto/AccountCreatedEvent.java
+++ notification-service/src/main/java/br/com/joaodddev/notification_service/dto/AccountCreatedEvent.java
@@ -1,4 +1,13 @@
 package br.com.joaodddev.notification_service.dto;
 
-public class AccountCreatedEvent {
-}
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record AccountCreatedEvent(
+        UUID accountId,
+        String ownerName,
+        String cpf,
+        BigDecimal balance,
+        LocalDateTime createdAt
+) {}
--- notification-service/src/main/java/br/com/joaodddev/notification_service/dto/TransferCompletedEvent.java
+++ notification-service/src/main/java/br/com/joaodddev/notification_service/dto/TransferCompletedEvent.java
@@ -1,4 +1,13 @@
 package br.com.joaodddev.notification_service.dto;
 
-public class TransferCompletedEvent {
-}
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record TransferCompletedEvent(
+        UUID transferId,
+        UUID sourceAccountId,
+        UUID targetAccountId,
+        BigDecimal amount,
+        LocalDateTime createdAt
+) {}
