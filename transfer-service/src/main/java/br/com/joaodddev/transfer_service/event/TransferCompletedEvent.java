03751ead70b570cb9b19ec5b51972b6f6a640dea
João Victor <joaodddev@gmail.com>
2026-06-12 09:40:01 -0300

    style(transfer): reorder imports in TransferEvent

--- transfer-service/src/main/java/br/com/joaodddev/transfer_service/event/TransferCompletedEvent.java
+++ transfer-service/src/main/java/br/com/joaodddev/transfer_service/event/TransferCompletedEvent.java
@@ -1,8 +1,8 @@
 package br.com.joaodddev.transfer_service.event;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
 import java.util.UUID;
+import java.time.LocalDateTime;
 
 public record TransferCompletedEvent(
         UUID transferId,
